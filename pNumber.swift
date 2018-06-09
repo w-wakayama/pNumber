3586612107e43f49d87643be25408362d775a6bb
w-wakayama <tsumamist715@gmail.com>
2018-06-09 17:40:42 +0900

    こっちの方がスマート

--- pNumber.swift
+++ pNumber.swift
@@ -1,20 +1,15 @@
 var n: Int = 331;
 
-var m = [Int]()
-for _ in (0..<n+1){
-  m.append(0)
-}
+var flag = 0;
 
-for i in (2 ..< n+1) {
-  for j in (2 ..< n/2) {
-    if i != j && i%j == 0 {
-      m[i] = 1
-      break
-    }
+for i in (2 ..< n) {
+  if n%i == 0 {
+    flag = 1;
+    break
   }
 }
 
-if m[n] == 0 {
+if flag == 0 {
   print("素数")
 } else {
   print("素数じゃない")
